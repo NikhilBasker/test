972ed590d825523ca6c4014a275784ab01e006b3
NikhilBasker <124427705+NikhilBasker@users.noreply.github.com>
2025-01-18 19:09:38 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-import os
+from app import create_app
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'my$up3r$3cur3K3y')
-    CORS(app)
+app = create_app()
 
-    from .routes import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
