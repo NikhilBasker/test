7bf1e7b4690e409ad0f66d96a3c512044846057c
NikhilBasker <124427705+NikhilBasker@users.noreply.github.com>
2025-01-18 19:53:35 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-import os
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
     CORS(app)
 
-    # Import and register the blueprint
     from .routes import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
